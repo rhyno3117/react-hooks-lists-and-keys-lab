a92f9508b1a5a7b5dda6e3a71f8fcc7232fddbae
Ryan Meyers <rhynogolfer42@gmail.com>
2023-08-21 13:06:39 -0600

    Adds myfile.md

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  //<a href="#home">home</a>
+  //Also has unique key prop
+  const anchors = links.map((link)=> (
+    <a key={link} href={"#" + link}>
+    {link}
+    </a>
+  ));
+  return <nav>{anchors}</nav>
 }
 
 export default NavBar;
